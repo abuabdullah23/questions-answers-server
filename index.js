66169249ddce590eb8ca7ac9ccc3e7d28c0fad28
abuabdullah23 <abuobaidazayed@gmail.com>
2023-07-10 15:32:14 +0600

    Add vercel.json file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -37,8 +37,19 @@ async function run() {
 
         // ================ questions and answer related api
         // get all questions and answer 
-        app.get('/questions-answer', async (req, res) => {
-            const result = await questionsAnswersCollection.find().toArray();
+        app.get('/questions-answers', async (req, res) => {
+            const query = {}
+            const options = {
+                sort: { _id: -1 }
+            }
+            const result = await questionsAnswersCollection.find(query, options).toArray();
+            res.send(result);
+        })
+
+        app.get('/qa-details/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)}
+            const result = await questionsAnswersCollection.findOne(query);
             res.send(result);
         })
 
